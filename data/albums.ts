ca4a5b41924423dc36579be8d8226f50582f1fbe
Mathdeep258 <3459238738@qq.com>
2026-08-30 22:36:00 +0800

    Auto sync source code for Vercel 🚀

--- data/albums.ts
+++ data/albums.ts
@@ -4,6 +4,21 @@ export interface Album { id: string; title: string; description: string; cover: 
 
 export const albums: Album[] = [
   {
+    "title": "蛊真人",
+    "cover": "https://pub-765df429105042d8a0d2a68ac29453aa.r2.dev/1788100336786-30911c8f-a94c-4cfe-a65c-ac0bdb44f6bb.webp",
+    "id": "album_1788100344813",
+    "photos": [],
+    "date": "2026-08-30"
+  },
+  {
+    "title": "诡秘之主",
+    "description": "注意：第一步",
+    "cover": "https://pub-765df429105042d8a0d2a68ac29453aa.r2.dev/1788100291636-e5503956-1ddb-4837-a8f3-72959d0a407c.webp",
+    "id": "album_1788100303079",
+    "photos": [],
+    "date": "2026-08-30"
+  },
+  {
     "title": "Sunset落日",
     "cover": "https://pub-765df429105042d8a0d2a68ac29453aa.r2.dev/1787718567348-38129cee-633e-4866-8870-d60301d1fc4c.jpg",
     "id": "album_1787718570823",
